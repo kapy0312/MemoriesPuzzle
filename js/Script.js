4824f7defdd8a2f43b549cbfe5c15ee13bdd48d1
kapy0312 <kapy0312@gmail.com>
2023-12-01 11:08:28 +0800

    20231201_v1

---
+++ js/Script.js
@@ -0,0 +1,26 @@
+
+$('#EmailButton').on('click', function () {
+
+    var Email_Name = $('#InputName').val();
+    var Email_Address = $('#InputEmail').val();
+    var Email_Contact = $('#contact_message').val();
+
+    Email_Contact = "寄信者："+Email_Name + "\n寄信者Email：" + Email_Address + "\n內容：\n" + Email_Contact;
+    Email_Name="回憶拼圖-系統訊息-意見回顧";
+    Email_Address="kapy0312@gmail.com"
+
+    $.ajax({
+        url: 'https://script.google.com/macros/s/AKfycbxsKiCXmBiUZwzHIkBsTW5C8z5wl3SEMDzqQGjklJpSRA_FzXDicTmWmx8liaAAMBDS/exec',
+        type: 'POST',
+        dataType: 'text',
+        data: JSON.stringify({ Name: Email_Name, Address: Email_Address, Contact: Email_Contact }),
+        contentType: 'text/plain; charset=utf-8',
+        success: function (response) {
+            alert(response); // 显示成功或错误消息
+        },
+        error: function () {
+            alert('Failed to write data.'); // 处理错误情况
+        }
+    });
+
+});
